d9000a22f2d75544b98cf6a5096fdca0729488d3
Rgardner25 <51680738+Rgardner25@users.noreply.github.com>
2019-09-03 10:19:06 -0500

    Create fliter.rb

---
+++ fliter.rb
@@ -0,0 +1,10 @@
+require 'rspec'
+
+class Array
+  def file_type_mapper
+    each_with_object(Hash.new { |h, k| h[k] = [] }) do |file, hash|
+      file_ext = File.extname(file)[1..-1]
+      hash[file_ext] = hash[file_ext].push(file.chomp!('.' + file_ext))
+    end
+  end
+end
